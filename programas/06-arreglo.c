27db52774308af1a5a316b31b8ebcb684a21dd90
tobi-ttdy-111 <torres.torres.daeg.yeray@gmail.com>
2022-11-18 11:46:11 -0600

    clase 11 creo

---
+++ programas/06-arreglo.c
@@ -0,0 +1,43 @@
+
+// includes
+#include <stdio.h>
+
+
+// main
+int main() {
+
+    // aqui leemos la longitud del arreglo
+    int longitud;
+    printf( "\nIngresa la longitud del arreglo: " );
+    scanf( "%i", &longitud );
+
+    // aqui creamos el arreglo
+    float numeros[ longitud ];
+
+    // aqui llenamos el arreglo dinamicamente
+    for ( int i = 0; i < longitud; i++ ) {
+        float numero;
+        printf( "numeros[ %i ] = ", i );
+        scanf( "%f", &numero );
+        numeros[ i ] = numero;
+    };
+
+    float valorActual;
+    float numeroMayor = 0;
+    for ( int i = 0; i < longitud; i++ ) {
+        valorActual = numeros[ i ];
+        if ( valorActual > numeroMayor ) {
+            numeroMayor = valorActual;
+        };
+    };
+
+    printf( "Valor mayor es: %f", numeroMayor );
+
+    // { 1, 3, 2, 6, 2, 9 }
+    // valorActual = 9
+    // numeroMayor = 9
+
+
+    return 0;
+
+};
